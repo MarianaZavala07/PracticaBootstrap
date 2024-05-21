79a65561f6888a2584a5db8a0f37813446a1420d
MarianaZavala07 <al44514@un.edu.mx>
2024-05-20 21:51:22 -0600

    Esto es una practica de BOOTSTRAP

---
+++ EXAMPLE_BOOTSTRAP/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EXAMPLE_BOOTSTRAP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
